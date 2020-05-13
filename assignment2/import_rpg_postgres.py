0d82352ededdd7fb8406601deafd41c34e21a68d
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-05-13 00:44:32 -0400

    Brings RPG data into PostgreSQL, minus necromancers and armory_weapons

--- assignment2/import_rpg_postgres.py
+++ assignment2/import_rpg_postgres.py
@@ -40,12 +40,12 @@ WHERE pg_tables.schemaname != 'pg_catalog' AND 'pg_tables.schemename' != 'inform
 # print(cur.fetchall())
 # print(chars[0])
 
-for character in chars:
-    insert_character = """
-    INSERT INTO charcreator_character
-    (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
-    VALUES """ + str(character[1:]) + ";"
-    cur.execute(insert_character)
+# for character in chars:
+#     insert_character = """
+#     INSERT INTO charcreator_character
+#     (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
+#     VALUES """ + str(character[1:]) + ";"
+#     cur.execute(insert_character)
 # conn.commit()
 
 # cur.execute('SELECT * FROM charcreator_character')
@@ -54,21 +54,21 @@ for character in chars:
 
 ###################### SUBCLASS: CLERIC #####################
 
-# Create cleric table
-cur.execute(
-    """
-    CREATE TABLE clerics (
-    character_ptr_id SERIAL PRIMARY KEY,
-    using_shield INTEGER NOT NULL,
-    mana INTEGER NOT NULL
-    );
-    """
-)
-conn.commit()
+### Create cleric table
+# cur.execute(
+#     """
+#     CREATE TABLE clerics (
+#     character_ptr_id SERIAL PRIMARY KEY,
+#     using_shield INTEGER NOT NULL,
+#     mana INTEGER NOT NULL
+#     );
+#     """
+# )
+# conn.commit()
 
-### Get and insert data for charcreator_cleric
-# get_characters = 'SELECT * FROM charactercreator_cleric'
-# chars = sl_curs.execute(get_characters).fetchall()
+### Get and insert data for clerics
+get_characters = 'SELECT * FROM charactercreator_cleric'
+chars = sl_curs.execute(get_characters).fetchall()
 
 # for character in chars:
 #     insert_character = """
@@ -78,5 +78,187 @@ conn.commit()
 #     cur.execute(insert_character)
 # conn.commit()
 
-# cur.execute('SELECT * FROM charcreator_cleric')
+# cur.execute('SELECT * FROM clerics')
 # cur.fetchall()
+
+
+###################### SUBCLASS: MAGE #####################
+
+### Create mage table
+# cur.execute(
+#     """
+#     CREATE TABLE mages (
+#     character_ptr_id SERIAL PRIMARY KEY,
+#     has_pet INTEGER NOT NULL,
+#     mana INTEGER NOT NULL
+#     );
+#     """
+# )
+# conn.commit()
+
+### Get and insert data for mages
+get_characters = 'SELECT * FROM charactercreator_mage'
+chars = sl_curs.execute(get_characters).fetchall()
+
+# for character in chars:
+#     insert_character = """
+#     INSERT INTO mages
+#     (has_pet, mana)
+#     VALUES """ + str(character[1:]) + ";"
+#     cur.execute(insert_character)
+# conn.commit()
+
+# cur.execute('SELECT * FROM mages')
+# cur.fetchall()
+
+
+###################### SUBCLASS: THIEF #####################
+
+### Create thieves table
+# cur.execute(
+#     """
+#     CREATE TABLE thieves (
+#     character_ptr_id SERIAL PRIMARY KEY,
+#     is_sneaking INTEGER NOT NULL,
+#     energy INTEGER NOT NULL
+#     );
+#     """
+# )
+# conn.commit()
+
+# ### Get and insert data for thieves
+# get_characters = 'SELECT * FROM charactercreator_thief'
+# chars = sl_curs.execute(get_characters).fetchall()
+
+# for character in chars:
+#     insert_character = """
+#     INSERT INTO thieves
+#     (is_sneaking, energy)
+#     VALUES """ + str(character[1:]) + ";"
+#     cur.execute(insert_character)
+# conn.commit()
+
+# cur.execute('SELECT * FROM thieves')
+# cur.fetchall()
+
+
+###################### SUBCLASS: FIGTHER #####################
+
+### Create fighters table
+# cur.execute(
+#     """
+#     CREATE TABLE fighters (
+#     character_ptr_id SERIAL PRIMARY KEY,
+#     using_shield INTEGER NOT NULL,
+#     rage INTEGER NOT NULL
+#     );
+#     """
+# )
+# conn.commit()
+
+# ### Get and insert data for fighters
+# get_characters = 'SELECT * FROM charactercreator_fighter'
+# chars = sl_curs.execute(get_characters).fetchall()
+
+# for character in chars:
+#     insert_character = """
+#     INSERT INTO fighters
+#     (using_shield, rage)
+#     VALUES """ + str(character[1:]) + ";"
+#     cur.execute(insert_character)
+# conn.commit()
+
+# cur.execute('SELECT * FROM fighters')
+# cur.fetchall()
+
+
+###################### SUBCLASS: NECROMANCERS #####################
+
+### Create necromancers table
+# cur.execute(
+#     """
+#     CREATE TABLE necromancers (
+#     character_ptr_id SERIAL PRIMARY KEY,
+#     talisman_charged INTEGER NOT NULL
+#     );
+#     """
+# )
+# conn.commit()
+
+### Get and insert data for necromancers
+# get_characters = 'SELECT * FROM charactercreator_necromancer'
+# chars = sl_curs.execute(get_characters).fetchall()
+
+# for character in chars:
+#     insert_character = """
+#     INSERT INTO necromancers
+#     (talisman_charged)
+#     VALUES """ + str(character[1]) + ";"
+#     # breakpoint()
+#     cur.execute(insert_character)
+# conn.commit()
+
+# cur.execute('SELECT * FROM necromancers')
+# cur.fetchall()
+# # Was actually unable to get this one to work - continued to get error
+# # regarding line 198 having a SyntaxError: at or near "1"
+
+
+###################### ITEMS #####################
+
+
+### Get and insert data for items
+# get_itms = 'SELECT * FROM armory_item'
+# itms = sl_curs.execute(get_itms).fetchall()
+
+# for itm in itms:
+#     insert_item = """
+#     INSERT INTO items
+#     (name, "value", weight)
+#     VALUES """ + str(itm[1:]) + ";"
+#     cur.execute(insert_item)
+# conn.commit()
+
+# cur.execute('SELECT * FROM items')
+# cur.fetchall()
+
+
+###################### SUBCLASS: WEAPONS #####################
+
+
+### Get and insert data for weapons
+# get_itms = 'SELECT * FROM armory_weapon'
+# itms = sl_curs.execute(get_itms).fetchall()
+
+# for itm in itms:
+#     insert_item = """
+#     INSERT INTO weapons
+#     (power)
+#     VALUES """ + str(itm[1:]) + ";"
+#     cur.execute(insert_item)
+# conn.commit()
+
+# cur.execute('SELECT * FROM weapons')
+# cur.fetchall()
+
+# # Ran into same error as necromancer subclass - must have
+# # something to do with itm[1:]
+
+
+###################### INVENTORY #####################
+
+
+### Get and insert data for char_inventory
+get_itms = 'SELECT * FROM charactercreator_character_inventory'
+itms = sl_curs.execute(get_itms).fetchall()
+
+for itm in itms:
+    insert_item = """
+    INSERT INTO char_inventory
+    (character_id, item_id)
+    VALUES """ + str(itm[1:]) + ";"
+    cur.execute(insert_item)
+conn.commit()
+
+cur.execute('SELECT * FROM char_inventory')
+cur.fetchall()
